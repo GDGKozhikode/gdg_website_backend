fa8e27315026d6221f252cd590f2820cc91d1450
Ligin Vellakkad <ligindd@gmail.com>
2018-02-17 02:30:07 +0530

    Creates models.

--- website/organizers/models.py
+++ website/organizers/models.py
@@ -1,3 +1,32 @@
+from django.core.validators import RegexValidator
 from django.db import models
+from social.models import SocialLinks
 
-# Create your models here.
+
+class Organizer(SocialLinks):
+    name = models.CharField(max_length=50, blank=False)
+
+    ORGANISER = "ORG"
+    CONTRIBUTOR = "CON"
+
+    DESIGINATION = (
+        (ORGANISER, 'organiser'),
+        (CONTRIBUTOR, 'contributor')
+    )
+
+    desigination = models.CharField(max_length=3,
+                                    choices=DESIGINATION,
+                                    default=CONTRIBUTOR)
+
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
+                                 message="Format: '+91999111999'")
+    contact_no = models.CharField(max_length=15,
+                                  validators=[phone_regex],
+                                  blank=False)
+
+    email = models.EmailField(max_length=70, blank=False, unique=True)
+
+    photo = models.ImageField(upload_to='organiser/', blank=True)
+
+    def __str__(self):
+        return self.name
