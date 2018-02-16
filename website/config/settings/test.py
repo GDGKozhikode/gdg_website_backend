658920b60a256540a4cfc84e4f68e347627e410c
Ligin Vellakkad <ligindd@gmail.com>
2018-02-17 00:03:30 +0530

    Create test suit for development.

---
+++ website/config/settings/test.py
@@ -0,0 +1,52 @@
+"""
+Test settings for website project.
+
+- Used to run tests fast on the continuous integration server and locally
+"""
+
+from .base import *  # noqa
+
+
+# DEBUG
+# ------------------------------------------------------------------------------
+# Turn debug off so tests run faster
+DEBUG = False
+TEMPLATES[0]['OPTIONS']['debug'] = False
+
+# SECRET CONFIGURATION
+# ------------------------------------------------------------------------------
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
+# Note: This key only used for development and testing.
+SECRET_KEY = env('DJANGO_SECRET_KEY',
+                 default='aJJDEhHqLdlPijfXLStVvnCznQQH2D2xTQkbdf1Qau0Mh2e3I8')
+
+# Mail settings
+# ------------------------------------------------------------------------------
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 1025
+
+# In-memory email backend stores messages in django.core.mail.outbox
+# for unit testing purposes
+EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
+
+# TESTING
+# ------------------------------------------------------------------------------
+TEST_RUNNER = 'django.test.runner.DiscoverRunner'
+
+
+# PASSWORD HASHING
+# ------------------------------------------------------------------------------
+# Use fast password hasher so tests run faster
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+]
+
+# TEMPLATE LOADERS
+# ------------------------------------------------------------------------------
+# Keep templates in memory so tests run faster
+TEMPLATES[0]['OPTIONS']['loaders'] = [
+    ['django.template.loaders.cached.Loader', [
+        'django.template.loaders.filesystem.Loader',
+        'django.template.loaders.app_directories.Loader',
+    ], ],
+]
