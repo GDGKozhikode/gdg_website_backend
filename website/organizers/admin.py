0048ba68bd377f6f501d2e8cb0420900f1ccb7e3
Ligin Vellakkad <ligindd@gmail.com>
2018-02-17 02:31:05 +0530

    Adds admin view of the app.

--- website/organizers/admin.py
+++ website/organizers/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Organizer
+
+
+class OrganizerAdmin(admin.ModelAdmin):
+    model = Organizer
+    list_display = ('name', 'desigination', 'contact_no', 'email')
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'desigination', 'contact_no', 'email', 'photo')
+        }),
+        ('Social Links', {
+            'fields': ('facebook', 'twitter', 'linkedin', 'google_plus')
+        }),
+    )
+
+
+admin.site.register(Organizer, OrganizerAdmin)
