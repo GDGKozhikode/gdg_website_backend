25d10ec9a226ba15f67f976538d238672803f323
Ligin Vellakkad <ligindd@gmail.com>
2018-02-18 11:59:08 +0530

    Adds test for name’s string representation

--- website/organizers/tests/test_models.py
+++ website/organizers/tests/test_models.py
@@ -1,1 +1,9 @@
 from test_plus.test import TestCase
+from ..models import Organizer
+
+
+class OrganizerModelTest(TestCase):
+
+    def test_string_representation(self):
+        organizer = Organizer(name="Ligin Vellakkad")
+        self.assertEqual(str(organizer), organizer.name)
